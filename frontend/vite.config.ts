907b571f616ddb31da0df5a40f1aa0ba709cbda1
Bruno L Nogueira <b.nogueira@protonmail.com>
2025-06-09 00:20:16 -0300

    add vite plugin checker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
+import checker from 'vite-plugin-checker'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    checker({ typescript: { ignoreDiagnostics: [] }, enableOverlay: false })
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
     },
   },
+  build: {
+    abortOnError: false
+  }
 })
